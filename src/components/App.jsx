49273587f0f31fa46ed5199bd18a18e55e3981d8
myinan <m.yasir.inan@gmail.com>
2024-03-23 16:03:02 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,11 @@
 import "../styles/App.css";
 
 function App() {
-  return <h1>Hello World.</h1>;
+  return (
+    <div className="app">
+      <h1>Hello World.</h1>
+    </div>
+  );
 }
 
 export default App;
